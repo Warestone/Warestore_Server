600b24b95654047581246d7f7d6aed1029b097a3
Eugene Timofeev <jontimofeev@yandex.ru>
2021-03-06 20:17:18 +0300

    refactor jdbc template & delete useless endpoints

--- src/main/java/org/warestore/controller/CatalogController.java
+++ src/main/java/org/warestore/controller/CatalogController.java
@@ -1,7 +1,5 @@
 package org.warestore.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,52 +14,25 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/server/catalog")
 public class CatalogController {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    private final CatalogService catalogService = new CatalogService();
+    private final CatalogService catalogService;
+
+    public CatalogController(CatalogService catalogService) { this.catalogService = catalogService; }
 
     @GetMapping(value = "/get/category")
-    public List<Category> getCategories(){ return catalogService.getCategories(jdbcTemplate); }
-
-    @GetMapping(value = "/get/rifle")
-    public List<Weapon>getRifles(){
-        return catalogService.getRifles(jdbcTemplate);
-    }
-
-    @GetMapping(value = "/get/shotgun")
-    public List<Weapon>getShotguns(){
-        return catalogService.getShotguns(jdbcTemplate);
-    }
-
-    @GetMapping(value = "/get/airgun")
-    public List<Weapon>getAirguns(){
-        return catalogService.getAirguns(jdbcTemplate);
-    }
-
-    @GetMapping(value = "/get/ammo")
-    public List<Ammo>getAmmo(){
-        return catalogService.getAmmo(jdbcTemplate);
-    }
-
-    @GetMapping(value = "/get/target")
-    public List<Target>getTarget(){
-        return catalogService.getTargets(jdbcTemplate);
-    }
-
-
+    public List<Category> getCategories(){ return catalogService.getCategories(); }
 
     @GetMapping(value = "/get/rifle_page/{id}")
-    public List<Weapon>getRiflesPage(@PathVariable int id){ return catalogService.getRiflesPage(jdbcTemplate, id); }
+    public List<Weapon>getRiflesPage(@PathVariable int id){ return catalogService.getRiflesPage(id); }
 
     @GetMapping(value = "/get/shotgun_page/{id}")
-    public List<Weapon>getShotgunsPage(@PathVariable int id){ return catalogService.getShotgunsPage(jdbcTemplate, id); }
+    public List<Weapon>getShotgunsPage(@PathVariable int id){ return catalogService.getShotgunsPage(id); }
 
     @GetMapping(value = "/get/airgun_page/{id}")
-    public List<Weapon>getAirgunsPage(@PathVariable int id){ return catalogService.getAirgunsPage(jdbcTemplate, id); }
+    public List<Weapon>getAirgunsPage(@PathVariable int id){ return catalogService.getAirgunsPage(id); }
 
     @GetMapping(value = "/get/ammo_page/{id}")
-    public List<Ammo>getAmmoPage(@PathVariable int id){ return catalogService.getAmmoPage(jdbcTemplate, id); }
+    public List<Ammo>getAmmoPage(@PathVariable int id){ return catalogService.getAmmoPage(id); }
 
     @GetMapping(value = "/get/target_page/{id}")
-    public List<Target>getTargetsPage(@PathVariable int id){ return catalogService.getTargetPage(jdbcTemplate, id); }
+    public List<Target>getTargetsPage(@PathVariable int id){ return catalogService.getTargetPage(id); }
 }
--- src/main/java/org/warestore/service/CatalogService.java
+++ src/main/java/org/warestore/service/CatalogService.java
@@ -17,9 +17,12 @@ import java.util.logging.Logger;
 
 @Service
 public class CatalogService {
+
+    public CatalogService(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
     private final Logger logger = Logger.getLogger(PostService.class.getName());
-    private final static String GET_WEAPON_OR_AMMO_QUERY = "select obj.id, obj.name, attr.name as type, param.value from objects obj, attributes attr, parameters param\n" +
-            "where param.object_id = obj.id and param.attribute_id = attr.id and attr.name !='product_type' and obj.parent_id = ";
+    private final JdbcTemplate jdbcTemplate;
+
     private final static String GET_WEAPON_OR_AMMO_PAGE_QUERY_1 = "select obj.id, obj.name, attr.name as type, param.value from objects obj, attributes attr, parameters param\n" +
             "where param.object_id = obj.id and param.attribute_id = attr.id and obj.parent_id = ";
     private final static String GET_WEAPON_OR_AMMO_PAGE_QUERY_2 = " and attr.name !='product_type' " +
@@ -28,8 +31,7 @@ public class CatalogService {
             "where param.object_id = obj.id and param.attribute_id = attr.id and obj.type_id = ";
 
 
-
-    public List<Category> getCategories(JdbcTemplate jdbcTemplate){
+    public List<Category> getCategories(){
         logger.info("Return categories.");
         if (!DataStorage.getCategoriesList().isEmpty()) return DataStorage.getCategoriesList();
         else{
@@ -40,50 +42,27 @@ public class CatalogService {
        return DataStorage.getCategoriesList();
     }
 
-
-    public List<Weapon> getRifles(JdbcTemplate jdbcTemplate){
-        logger.info("Return all rifles.");
-        return jdbcTemplate.query(GET_WEAPON_OR_AMMO_QUERY+ Categories.RIFLES.ordinal(), new WeaponMapper());
-    }
-    public List<Weapon> getShotguns(JdbcTemplate jdbcTemplate){
-        logger.info("Return shotguns.");
-        return jdbcTemplate.query(GET_WEAPON_OR_AMMO_QUERY+Categories.SHOTGUNS.ordinal(), new WeaponMapper());
-    }
-    public List<Weapon> getAirguns(JdbcTemplate jdbcTemplate){
-        logger.info("Return airguns.");
-        return jdbcTemplate.query(GET_WEAPON_OR_AMMO_QUERY+Categories.AIRGUNS.ordinal(), new WeaponMapper());
-    }
-    public List<Ammo> getAmmo(JdbcTemplate jdbcTemplate){
-        logger.info("Return ammo.");
-        return jdbcTemplate.query(GET_WEAPON_OR_AMMO_QUERY+Categories.AMMO.ordinal(), new AmmoMapper());
-    }
-    public List<Target> getTargets(JdbcTemplate jdbcTemplate){
-        logger.info("Return targets.");
-        return jdbcTemplate.query(GET_WEAPON_OR_AMMO_QUERY+Categories.TARGETS.ordinal(), new TargetMapper());
-    }
-
-
-    public List<Weapon> getRiflesPage(JdbcTemplate jdbcTemplate, int page){
+    public List<Weapon> getRiflesPage(int page){
         logger.info("Return rifles page "+page);
         return jdbcTemplate.query(GET_WEAPON_OR_AMMO_PAGE_QUERY_1+Categories.RIFLES.ordinal()+
                 GET_WEAPON_OR_AMMO_PAGE_QUERY_2+page*20, new WeaponMapper());
     }
-    public List<Weapon> getShotgunsPage(JdbcTemplate jdbcTemplate, int page){
+    public List<Weapon> getShotgunsPage(int page){
         logger.info("Return shotguns page "+page);
         return jdbcTemplate.query(GET_WEAPON_OR_AMMO_PAGE_QUERY_1+Categories.SHOTGUNS.ordinal()+
                 GET_WEAPON_OR_AMMO_PAGE_QUERY_2+page*20, new WeaponMapper());
     }
-    public List<Weapon> getAirgunsPage(JdbcTemplate jdbcTemplate, int page){
+    public List<Weapon> getAirgunsPage(int page){
         logger.info("Return airguns page "+page);
         return jdbcTemplate.query(GET_WEAPON_OR_AMMO_PAGE_QUERY_1+Categories.AIRGUNS.ordinal()+
                 GET_WEAPON_OR_AMMO_PAGE_QUERY_2+page*20, new WeaponMapper());
     }
-    public List<Ammo> getAmmoPage(JdbcTemplate jdbcTemplate, int page){
+    public List<Ammo> getAmmoPage(int page){
         logger.info("Return ammo page "+page);
         return jdbcTemplate.query(GET_WEAPON_OR_AMMO_PAGE_QUERY_1+Categories.AMMO.ordinal()+
                 GET_WEAPON_OR_AMMO_PAGE_QUERY_2+page*20, new AmmoMapper());
     }
-    public List<Target> getTargetPage(JdbcTemplate jdbcTemplate, int page){
+    public List<Target> getTargetPage(int page){
         logger.info("Return ammo page "+page);
         return jdbcTemplate.query(GET_WEAPON_OR_AMMO_PAGE_QUERY_1+Categories.TARGETS.ordinal()+
                 GET_WEAPON_OR_AMMO_PAGE_QUERY_2+page*20, new TargetMapper());
