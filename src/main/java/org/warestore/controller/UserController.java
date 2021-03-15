1bb3f585fbc779b8636eb10aa45cf5a7cfc2ead4
Eugene Timofeev <jontimofeev@yandex.ru>
2021-03-15 14:03:41 +0300

    add login, registration token's logic

--- src/main/java/org/warestore/controller/UserController.java
+++ src/main/java/org/warestore/controller/UserController.java
@@ -1,6 +1,9 @@
 package org.warestore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.warestore.configuration.jwt.JwtProvider;
 import org.warestore.model.User;
@@ -8,6 +11,8 @@ import org.warestore.model.UserAuthentication;
 import org.warestore.model.UserRegistration;
 import org.warestore.service.UserService;
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/server/user")
@@ -18,25 +23,26 @@ public class UserController {
     @Autowired
     private JwtProvider jwtProvider;
 
-    @GetMapping(value = "/get/{username}")
-    public User getUserByUsername(@PathVariable String username){
-        return userService.getUserByName(username);
+    @GetMapping(value = "/get/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username){
+        User user =  userService.getUserByName(username);
+        if (user == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping(value = "/register")
-    public UserRegistration registerUser(@RequestBody @Valid UserRegistration userRegistration){
-        if (userService.getUserByName(userRegistration.getUsername())==null){
-            userService.saveUser(userRegistration);
-            return userRegistration;
-        }
-        else return null;
+    public List<String> registerUser(@RequestBody @Valid UserRegistration userRegistration){
+        UserRegistration user = userService.saveUser(userRegistration);
+        if (user == null) return null;
+        else return authUser(new UserAuthentication(userRegistration.getUsername(),
+                userRegistration.getPassword()));
     }
 
     @PostMapping(value = "/auth")
-    public String authUser(@RequestBody @Valid UserAuthentication userAuthentication){
+    public List<String> authUser(@RequestBody @Valid UserAuthentication userAuthentication){
         User user = userService.getUserByNameAndPassword(userAuthentication.getUsername(),
                 userAuthentication.getPassword());
-        if (user==null) return "user not found";
-        return jwtProvider.generateToken(user.getUsername());
+        if (user==null) return null;
+        else return Collections.singletonList(jwtProvider.generateToken(user.getUsername()));
     }
 }
--- src/main/java/org/warestore/model/UserAuthentication.java
+++ src/main/java/org/warestore/model/UserAuthentication.java
@@ -1,9 +1,11 @@
 package org.warestore.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@AllArgsConstructor
 public class UserAuthentication {
     @NotEmpty
     private String username;
